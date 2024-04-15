77a010d25d5a94c0613d4830f52b2fb0d49fc416
CaraRota <canalsdigital@gmail.com>
2024-04-14 22:11:12 -0300

    Change installments formula

--- src/config/calculateInstallments.js
+++ src/config/calculateInstallments.js
@@ -1,5 +1,5 @@
 export const calculateInstallment = (price, cuotas, porcentaje, fijo) => {
-    const totalCuotas = (price * (1 + porcentaje) + parseFloat(fijo)) / cuotas;
+    const totalCuotas = ((price + parseFloat(fijo)) * (1 + porcentaje)) / cuotas;
     const parsedCuotas = totalCuotas.toLocaleString("es-AR", {
         style: "currency",
         currency: "ARS",
